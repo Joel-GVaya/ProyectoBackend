f59ac1753dd4e2feeecf3dcaca5c3487b146e6b7
atiribeja04 <atiribeja04@gmail.com>
2025-02-23 23:54:13 +0100

    CORS actualizado para funcionar con nueva app desplegada

--- config/cors.php
+++ config/cors.php
@@ -2,33 +2,23 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Aquí puedes configurar tus ajustes para el intercambio de recursos de
-    | origen cruzado (CORS). Esto determina qué operaciones se pueden
-    | ejecutar en los navegadores web.
-    |
-    | Más información: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    'paths' => ['*'], 
 
-    'paths' => ['*'], // Permite todas las rutas
+    'allowed_methods' => ['*'], 
 
-    'allowed_methods' => ['*'], // Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    'allowed_origins' => [
+        'https://www.projectegrup3.ddaw.es',
+        'https://front.projectegrup3.ddaw.es',
+    ], 
 
-    'allowed_origins' => ['*'], // Permite todos los orígenes
+    'allowed_origins_patterns' => [], 
 
-    'allowed_origins_patterns' => [], // No se restringen patrones de origen
+    'allowed_headers' => ['*'], 
 
-    'allowed_headers' => ['*'], // Permite todos los encabezados
+    'exposed_headers' => [], 
 
-    'exposed_headers' => [], // No expone encabezados adicionales
+    'max_age' => 0, 
 
-    'max_age' => 0, // No establece tiempo máximo de caché para preflight
-
-    'supports_credentials' => true, // Permite el envío de credenciales (cookies, headers de autenticación, etc.)
+    'supports_credentials' => true,
 
 ];
