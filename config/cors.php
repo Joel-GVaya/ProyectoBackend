bae2eae6269e0544c94df37c88816f03adfcadcb
atiribeja04 <atiribeja04@gmail.com>
2025-02-24 01:04:07 +0100

    Último retoque de CORS

--- config/cors.php
+++ config/cors.php
@@ -6,15 +6,15 @@ return [
 
     'allowed_methods' => ['*'], 
 
-    'allowed_origins' => ['*'], 
+    'allowed_origins' => ['http://localhost:5174', 'https://front.projectegrup3.ddaw.es', 'https://www.projectegrup3.ddaw.es'],
 
     'allowed_origins_patterns' => [], 
 
     'allowed_headers' => ['*'], 
 
-    'exposed_headers' => [], 
+    'exposed_headers' => ['*'], 
 
-    'max_age' => 0, 
+    'max_age' => 7200, 
 
     'supports_credentials' => true,
 
